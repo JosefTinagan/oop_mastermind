b3db4bb3e414b0ec402f03a0f252b6acba0956ca
Josef Perry Tinagan <josephperry_06@yahoo.com>
2016-11-07 23:16:13 +0800

    choosing mode updated

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,3 +1,30 @@
 class Mastermind
+	attr_accessor :name, :mode
 
+	def initialize(name="You")
+		@name = name
+		#@mode = choosing_mode
+	end
+
+	private
+
+	def choosing_mode
+		puts "Would you like to play as a code breaker or as a code master?"
+		puts "[1] to play as a code breaker"
+		puts "[2] to play as a code master"
+		choice = gets.chomp
+		mode_chose(choice.to_i)
+	end
+
+	def mode_chose(choice)
+
+		if choice == 1
+			@mode = "BREAKER"
+		elsif choice == 2
+			@mode = "MASTER"
+		else
+			puts "Invalid choice, try again..."
+			choosing_mode
+		end
+	end
 end
---
+++ spec/lib/mastermind_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+require 'mastermind'
+
+describe Mastermind do
+	mm = Mastermind.new
+	mm2 = Mastermind.new("Jake")
+
+	it "initializes with a default name" do
+		param = mm.name
+		expect(param).to eq("You")
+	end
+
+	it "initializes with an optional name" do
+		param = mm2.name
+		expect(param).to eq("Jake")
+	end
+
+	it "can choose game mode breaker" do
+		mm.send(:mode_chose,1)
+		param = mm.mode
+		expect(param).to eq("BREAKER")
+	end
+
+	it "can choose game mode master" do
+		mm2.send(:mode_chose,2)
+		param = mm2.mode
+		expect(param).to eq("MASTER")
+	end
+
+	it "can choose game mode by user input" do
+		mm.send(:choosing_mode)
+		param = mm.mode
+		expect(param).to eq("BREAKER")
+	end
+
+
+end
